b75d7cbf4d39af553843857bb0e40f2c3d4c1dbf
alpertunga-bile <kingdom19online@hotmail.com>
2024-07-02 09:42:34 +0300

    fmt lib is added

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
-#include <stdio.h>
+#include "core.h"
 
 #include "ispc_shaders/simple.h"
 using namespace ispc;
 
-int
-main()
-{
+int main() {
   float vin[16], vout[16];
 
   for (int i = 0; i < 16; i++) {
@@ -15,5 +13,5 @@ main()
   simple(vin, vout, 16);
 
   for (int i = 0; i < 16; ++i)
-    printf("%d: simple(%f) = %f\n", i, vin[i], vout[i]);
+    fmt::println("{}: simple({}) = {}", i, vin[i], vout[i]);
 }
